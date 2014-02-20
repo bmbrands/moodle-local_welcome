3530ac739a6e4f8f000504696b8758be564bafb1
Bas Brands <bas@sonsbeekmedia.nl>
2014-02-20 09:35:15 +0100

    fixed lang error

--- lang/en/local_welcome.php
+++ lang/en/local_welcome.php
@@ -31,7 +31,7 @@ $string['pluginname'] = 'Moodle welcome';
 $string['message_user_enabled'] = 'Enable user messages';
 $string['message_user_enabled_desc'] = 'This tickbox enables the sending of welcome messages to new users';
 $string['message_user_subject'] = 'User subject';
-$string['message_user_subject_desc'] = 'This will be the subject of the email send to the user. Use [[user]] as a tag, this will be replace with the users Firstname Lastname.';
+$string['message_user_subject_desc'] = 'This will be the subject of the email send to the user. Use [[fullname]] as a tag, this will be replace with the users Firstname Lastname.';
 $string['message_user'] = 'User message';
 $string['message_user_desc'] = 'Message send to new users';
 
@@ -39,7 +39,7 @@ $string['message_moderator_enabled'] = 'Enable moderator messages';
 $string['message_moderator_enabled_desc'] = 'This tickbox enables the sending of notification messages to moderators';
 $string['message_moderator'] = 'Moderator message';
 $string['message_moderator_subject'] = 'Moderator subject';
-$string['message_moderator_subject_desc'] = 'This will be the subject of the email send to the moderator. Use [[user]] as a tag, this wil be replace with the users Firstname Lastname.';
+$string['message_moderator_subject_desc'] = 'This will be the subject of the email send to the moderator. Use [[fullname]] as a tag, this wil be replace with the users Firstname Lastname.';
 $string['message_moderator_desc'] = 'Message send to moderators';
 $string['moderator_email'] = 'Moderator email';
 $string['moderator_email_desc'] = 'New user notifications are send to this email address';
@@ -53,12 +53,12 @@ $string['sender_firstname_desc'] = 'First name used when sending mail to users.'
 $string['sender_lastname'] = 'Moderator lastname';
 $string['sender_lastname_desc'] = 'Last name used when sending mail to users.';
 
-$string['default_user_email_subject'] = 'Hello [[user]] Welcome to {$a}';
-$string['default_user_email'] = 'Hello [[user]],
+$string['default_user_email_subject'] = 'Hello [[fullname]] Welcome to {$a}';
+$string['default_user_email'] = 'Hello [[fullname]],
 
     Thanks for creating an account on {$a}';
-$string['default_moderator_email_subject'] = 'A new user signed up on {$a} : [[user]]';
+$string['default_moderator_email_subject'] = 'A new user signed up on {$a} : [[fullname]]';
 $string['default_moderator_email'] = 'Hi moderator,
 
-  A new user: [[user]], has signed up for {$a}';
+  A new user: [[fullname]], has signed up for {$a}';
 
